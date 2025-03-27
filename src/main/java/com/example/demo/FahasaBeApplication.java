98566aedb86acbde5eb529379c03db8080950331
NgoThienPhu <thienphu1234@gmail.com>
2025-03-27 15:22:36 +0700

    khoi tao du an fahasa-be

---
+++ src/main/java/com/example/demo/FahasaBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FahasaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FahasaBeApplication.class, args);
+	}
+
+}
